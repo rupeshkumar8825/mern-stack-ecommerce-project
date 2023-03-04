a826af02bbe3742853f256dc892241b949caf8c4
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2023-03-04 11:19:08 +0530

    made a simple server for the backend

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+// this is the file to use the routes and define the endpoint for first point for great file structure 
+console.log("hi this is express app file ")
+// const app = require('./server')
+// import app from "./server"
+const express = require('express');
+const app = express();
+
+
+// say everything went fine 
+module.exports =  app;
+
+
+// SAY EVERYTHING WENT FINE 
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
 console.log("Hi this is server side of the ecommerce application for learning purpose")
+const app = require("./app")
+const dotenv = require('dotenv')
+
+// here we are telling to use this env to be able to fetch the value of the PORT 
+// do note that this PORT value will be not pushed to the repo as env files are kind of hidden 
+dotenv.config({path : "backend/config/config.env"})
+
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server  is running or working on http://localhost/${process.env.PORT}`)
+})
