18fdc2011b6f9650c938f8c2a343edcca2dc9c7e
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2023-03-09 16:10:17 +0530

    Added and tested the route for updating the product details for this purpose

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,6 +3,7 @@ const Product = require("../models/productModel")
 
 // we have to define the function to create the product for this purpose 
 // we have to use the async and await for this purpose
+// this will be admin route to create the new product for this purpose 
 exports.createProduct = async (req, res, next) => {
     console.log("hi i am creating the product for this purpose");
     const product = await Product.create(req.body)
@@ -16,8 +17,40 @@ exports.createProduct = async (req, res, next) => {
 }
 
 // this is what we follow the design pattern to not to over write the code in a single file 
-exports.getAllProducts = async (req, res) =>{
+exports.getAllProducts = async (req, res, next) =>{
     const products = await Product.find()
     // say everything went fine 
     res.status(200).json({message : "Route is working fine", products: products})
+}
+
+// function to update the product for this purpose 
+// this will be admin route 
+exports.updateProduct = async(req, res, next) => {
+    console.log("The user is updating the product details for this purpose \n");
+
+    const currentId = req.params.id;
+    let currentProduct = await Product.findById(currentId);
+
+    // if product is not found 
+    if(!currentProduct)
+    {
+        return res.status(500).json({
+            success : false,
+            message : "Product not found for this purpose"
+        })
+    }
+
+    // otherwise we have to update this product 
+    currentProduct = await Product.findByIdAndUpdate(currentId, req.body, {
+        new : true, 
+        runValidators : true, 
+        useFindAndModify : false
+    });
+
+
+    
+
+    // say everything went fine 
+    res.status(200).json({message : "Successfully Updated the product for this purpose", product : currentProduct});
+
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,13 @@
 // this is the file to define and put the routes related to product for following the design pattern 
 
 const express = require('express');
-const { getAllProducts, createProduct } = require('../controllers/productController');
+const { getAllProducts, createProduct, updateProduct } = require('../controllers/productController');
 const router = express.Router();
 
 
 // now we have to define the routes for this product for this puropse 
 router.route("/products").get(getAllProducts)
 router.route("/product/new").post(createProduct)
+router.route("/product/:id").put(updateProduct)
 
 module.exports = router
